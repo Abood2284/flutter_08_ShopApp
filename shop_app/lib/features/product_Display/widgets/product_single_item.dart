25144c3522863b4029a80369b611886f093e2e43
abood <sayyedabood69@gmail.com>
2022-02-27 18:15:25 +0530

    MAde cahnges to the course

--- shop_app/lib/features/product_Display/widgets/product_single_item.dart
+++ shop_app/lib/features/product_Display/widgets/product_single_item.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
 import '../screens/product_details_screen.dart';
+import '../../cart/providers/cart.dart';
 
 class ProductSingleItem extends StatelessWidget {
   // final String id;
@@ -15,13 +16,14 @@ class ProductSingleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(15),
       child: GridTile(
         child: GestureDetector(
             onTap: () {
-              Navigator.of(context)
-                  .pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
             },
             child: Image.network(product.imageUrl, fit: BoxFit.cover)),
         footer: GridTileBar(
@@ -42,7 +44,9 @@ class ProductSingleItem extends StatelessWidget {
             overflow: TextOverflow.fade,
           ),
           trailing: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.title, product.price);
+              },
               icon: Icon(Icons.shopping_cart,
                   color: Theme.of(context).accentColor)),
         ),
