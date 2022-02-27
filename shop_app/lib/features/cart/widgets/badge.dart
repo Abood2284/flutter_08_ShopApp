f1eae5727913d7709136a0ae535131df3623df6d
abood <sayyedabood69@gmail.com>
2022-02-27 18:14:58 +0530

    File provided by Max in lecture 203 for adding a text above the Cart icon this file is called in product_overview_screen.dart

---
+++ shop_app/lib/features/cart/widgets/badge.dart
@@ -0,0 +1,50 @@
+/// ! This class & file is not created by my it was created by max in udemy lecutre 203, so that we can display a count on our cart icon how many items were added, since flutter dont provide any such Widget.
+
+import 'package:flutter/material.dart';
+
+class Badge extends StatelessWidget {
+
+  final Widget child;
+  final String value;
+  final Color? color;
+  
+  const Badge({
+    Key? key,
+    required this.child,
+    required this.value,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        child,
+        Positioned(
+          right: 8,
+          top: 8,
+          child: Container(
+            padding: const EdgeInsets.all(2.0),
+            // color: Theme.of(context).accentColor,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: color != null ? color : Theme.of(context).accentColor,
+            ),
+            constraints: const BoxConstraints(
+              minWidth: 16,
+              minHeight: 16,
+            ),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 10,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
