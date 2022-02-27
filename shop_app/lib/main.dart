7c9cb8da49a840e5e5ef7365800b740a830cbb8a
abood <sayyedabood69@gmail.com>
2022-02-27 18:13:14 +0530

    addded another screen to route

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -4,15 +4,25 @@ import 'package:provider/provider.dart';
 import 'features/product_Display/screens/products_overview_screen.dart';
 import 'features/product_Display/screens/product_details_screen.dart';
 import 'features/product_Display/providers/products.dart';
+import './features/cart/providers/cart.dart';
+import './features/cart/screens/cart_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) =>
-          Products(), // Setting up the provider in the root widget because we want access to ProductsOverviewScreen & ProductDetailScreen which are instantiated here
+    // Thata how you can add mutiple providers without making code cumbersum
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) =>
+              Products(), // Setting up the provider in the root widget because we want access to ProductsOverviewScreen & ProductDetailScreen which are instantiated here
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
@@ -22,6 +32,7 @@ class MyApp extends StatelessWidget {
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
         },
       ),
     );
