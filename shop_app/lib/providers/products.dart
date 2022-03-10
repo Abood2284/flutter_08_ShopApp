ce8cc7412620839a28ea470937041314305a2d5d
abood <sayyedabood69@gmail.com>
2022-03-10 19:21:17 +0530

    Working on errro handling and http requests

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -64,16 +64,16 @@ class Products with ChangeNotifier {
     /// sTORING OUR FIREBASE URL IN A VARIABLE THIS WILL BE CONVERTED TO QUERY BY FLUTTER.
     /// In fireBase url you can create folders by adding /folder-name.json as this will be converted to quert which will execute and create a folder if not there already-> here products.json folder is created inside that we post
     final url = Uri.parse(
-        'https://flutter-08-shopapp-udemy-default-rtdb.firebaseio.com/products');
+        'https://flutter-08-shopapp-udemy-default-rtdb.firebaseio.com/products.json');
 
     /// we imported our http.dart as http
     /// .post takes in url where you want to post and some named args,
     /// body defines how and what your db should have.
     /// as body and everything on backend works on json so we import dart:converter to import our dart to json.
     /// As you know Js more works with maps some key and value that is why we tried to pass maps which suited us, though body takes dynamic value
-    /// 
+    ///
     /// Since we made this Future we need to return something, also .then returns us a future which makes dart satisfied
-   return http
+    return http
         .post(url,
             body: json.encode({
               'title': product.title,
@@ -82,22 +82,26 @@ class Products with ChangeNotifier {
               'imageUrl': product.imageUrl,
               'isFavorite': product.isFavorite,
             }))
-            /// * Http requests runs async(this will execute but it will not wait for response it will continue it's execution of code below it) so if you want to wait to get back the response then use Future returned by post
+
+        /// * Http requests runs async(this will execute but it will not wait for response it will continue it's execution of code below it) so if you want to wait to get back the response then use Future returned by post
         .then((response) {
-          logger.d('Response Recieved');
+      logger.d('Response Recieved');
       // Firebase sends us the response which is the cryptic id you see infront of your products.json to see this
-      logger.d(json.decode(response.body)); // It will print id of the product which just added though it will in map
+      logger.d(json.decode(response
+          .body)); // It will print id of the product which just added though it will in map
       final newProduct = Product(
         title: product.title,
         description: product.description,
         price: product.price,
         imageUrl: product.imageUrl,
-        id: json.decode(response.body)['name'], // as it is map, how i know? when you print logger you can see   And now we can use our Firebase Backend unique Id generated in our frontEnd also, this will help us in deleting item in the backend as well as frontend
+        id: json.decode(response.body)[
+            'name'], // as it is map, how i know? when you print logger you can see   And now we can use our Firebase Backend unique Id generated in our frontEnd also, this will help us in deleting item in the backend as well as frontend
       );
       _items.add(newProduct);
       notifyListeners(); // * This will notify our child listeners about the data changed once we add new product to the list
-      return Future.value();// Returning Future
+      return Future.value(); // Returning Future
     }).catchError((error) {
+      logger.d(error);
       throw error; // becuase this is not our widget class and we cannot do much here so we throw trh error and catch this error in edit_product_screen were we could do something about this error
     });
   }
